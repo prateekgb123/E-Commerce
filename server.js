fc3f579bb844455c88119a1f070678c031924ece
prateekgb123 <prateekgb2004@gmail.com>
2025-04-05 09:42:21 +0530

    minor changes

--- server.js
+++ server.js
@@ -109,4 +109,4 @@ app.post('/confirm-order', authenticateToken, async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on https://e-commerce-zyfc.onrender.com`));
+app.listen(PORT, () => console.log(`Server running on {PORT}`));
